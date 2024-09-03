58abed978e9557f276b1493d207060ec5f5939e4
JamesJames1231 <153459777+JamesJames1231@users.noreply.github.com>
2024-09-03 11:18:27 +0100

    Removed the default  api and added the custom one

---
+++ UserAuthApi/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserAuthApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost(Name = "UserAuth")]
+        public string Post()
+        {
+            return "heheh";
+        }
+    }
+}
