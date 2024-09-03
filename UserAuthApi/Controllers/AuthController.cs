eec1bf92d122d3008ef03ff7eca1a795d5b6f8aa
JamesJames1231 <153459777+JamesJames1231@users.noreply.github.com>
2024-09-03 11:30:52 +0100

    Added the input strings

--- UserAuthApi/Controllers/AuthController.cs
+++ UserAuthApi/Controllers/AuthController.cs
@@ -7,11 +7,13 @@ namespace UserAuthApi.Controllers
     public class AuthController : ControllerBase
     {
         [HttpPost(Name = "UserAuth")]
-        public bool Post()
+        public bool Post([FromBody] string username, [FromBody] string password)
         {
+            //Accepting string passwords over HTTPS appears to be the industry standard, which is good as it means I do not have to encrypt in every app I build
+            //Calls my auth function
             AuthMessage authMess = new AuthMessage();
-            bool response = authMess.CheckLogin("gdes", "test");
-            return response; ;
+            bool response = authMess.CheckLogin(username, password);
+            return response;
         }
     }
 }
